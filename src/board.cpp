5b51ac8e13b10c3c95002386c8aa322119424f9d
G-alenc <ufmgalencastro@gmail.com.com>
2025-05-20 17:57:27 -0300

    adiciona o metodo reset_board()

--- src/board.cpp
+++ src/board.cpp
@@ -1,1 +1,13 @@
 #include "board.hpp"
+
+Board::Board(){
+
+}
+
+void Board::reset_board(){
+    for (auto line : board){
+        for (Cell cell : line){
+            if(cell.is_fixed()) cell.set_value(0);
+        }
+    }
+}
