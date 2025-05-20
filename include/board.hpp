bf712a131b99831dbb4ed2859d955f5436bc4231
G-alenc <ufmgalencastro@gmail.com.com>
2025-05-20 17:57:27 -0300

    adiciona o metodo reset_board()

--- include/board.hpp
+++ include/board.hpp
@@ -6,14 +6,12 @@
 using namespace std;
 
 class Board{
-
     private:
-
-
+        // array de arrays de celulas que formam o tabuleiro
+        array<array<Cell, 9>, 9> board;
 
     public:
-        // lista das 81 células
-        array<Cell, 81> board;
+        Board();
 
         // retorna o numero de células preenchidas
         int cells_filled() const;
@@ -21,14 +19,14 @@ class Board{
         // retorna 'true' se o tabuleiro estiver completo
         bool is_completed() const;
 
-        // apaga todas as células que tem 'fixed == ture'
+        // apaga todas as células que tem 'fixed == false'
         void reset_board();
 
         // acessa o valor de uma celula pela sua posição
-        int value(int x, int y) const;
+        int get_value(int x, int y) const;
 
         // edita o valor de uma celula dada uma posição
-        int chance_value(int x, int y, int value);
+        int change_value(int x, int y, int value);
 
 };
 
